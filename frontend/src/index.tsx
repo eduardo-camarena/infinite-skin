35cac6851fd73c20746f3c08504999b13af348f9
Eduardo Camarena <lalo.a.camarena@gmail.com>
2024-05-17 13:20:47 -0600

    fixed eslintrc

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,17 +8,16 @@ import App from './App';
 const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?'
-  );
+throw new Error(
+'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
+);
 }
 
 render(
-  () => (
-    <Router>
-      <App />
-    </Router>
-  ),
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  root!
+() => (
+<Router>
+<App />
+</Router>
+),
+root!,
 );
